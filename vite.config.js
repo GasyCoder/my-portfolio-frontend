53a16e4f4a2c71556e98fb64b3b0dffcd11c42e9
GasyCoder <gasikaradigital@gmail.com>
2025-03-31 20:18:52 +0300

    chore: update dependencies for improved performance and compatibility; enhance Vite configuration with Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
