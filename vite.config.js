5e5c1f81c28e626b9dc083d49f7653b4663444d7
GasyCoder <gasikaradigital@gmail.com>
2025-03-31 13:56:44 +0300

    feat: add PWA support with service worker and update favicon files

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,38 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BEZARA Florent',
+        short_name: 'Portfolio',
+        description: 'My Portfolio - BEZARA Florent',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/img/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
